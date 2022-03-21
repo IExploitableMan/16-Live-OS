d8a77c41f0db20f755ab5c7bf7dd9deb1294d45e
pythonblack5000 <pythonblack5000@gmail.com>
2022-03-21 15:33:17 +0300

    Пробелы -> табы; Класс команд

--- Kernel.cs
+++ Kernel.cs
@@ -5,17 +5,35 @@ using SystemOS = Cosmos.System;
 
 namespace OS
 {
-    public class Kernel : SystemOS.Kernel
-    {
+	public class Kernel : SystemOS.Kernel
+	{
 
-        protected override void BeforeRun()
-        {
+		protected override void BeforeRun()
+		{
 
-        }
+		}
 
-        protected override void Run()
-        {
-            Console.Write("Загруженно.");
-        }
-    }
+		protected override void Run()
+		{
+			Command[] commands =
+			{
+				new Command("помогите", "Даёт никому не нужную помощь", new List<string> { "хелп" }),
+			};
+			Console.Write("Загруженно.");
+		}
+	}
+
+	public class Command 
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public List<string> Aliases { get; set; }
+
+		public Command(string _name, string _description, List<string> aliases) 
+		{
+			Name = _name;
+			Description = _description;
+			Aliases = aliases;
+		}
+	}
 }
