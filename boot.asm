9f2a05f7cba774d064bf6d114283bb264af0c454
pythonblack5000 <pythonblack5000@gmail.com>
2022-03-22 15:20:38 +0300

    ...

--- boot.asm
+++ boot.asm
@@ -1,11 +1,4 @@
-org 7C00h
-cli              ;Запрещаем прерывания (чтобы ничего не отвлекало)
-xor ax, ax       ;Обнуляем регистр ax
-mov ds, ax       ;Настраиваем dataSegment на нулевой адрес
-mov es, ax       ;Настраиваем сегмент es на нулевой адрес
-mov ss, ax       ;Настраиваем StackSegment на нулевой адрес
-mov sp, 07C00h   ;Указываем на текущую вершину стека
-sti              ;Запрещаем прерывания
+[BITS 16]
 
 ;Очищаем экран
 mov ax, 3
@@ -16,6 +9,8 @@ mov dh, 0
 mov dl, 0
 xor bh, bh
 int 10h
+
+%include "kernel.asm"
 
 jmp $
 times 510 - ($ - $$) db 0
--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
 @echo off
 start nasm/nasm.exe -f bin -o boot.bin boot.asm
-copy boot.bin boot.mbr
 pause
