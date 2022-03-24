9261bb4d99892a04a4358d88dd0a4422919f22ed
Space-Vest-Studio <pythonblack5000@gmail.com>
2022-03-24 17:31:54 +0300

    добавил команды h и s

--- kernel.asm
+++ kernel.asm
@@ -14,17 +14,30 @@ loop:
 
 	call read_key
 
-	mov dl, 4h
-	mov cx, 1h
+	mov dl, invite_string_end - invite_string - 01h
+	mov cx, 01h
 	mov bp, input_char
 	call print
 
 	call carriage_return
 
-jmp loop
+	mov ah, [input_char]
 
-;заморозка программы
-jmp $
+	cmp ah, [help_command_key]
+	je _help_command
+
+	cmp ah, [shut_command_key]
+	je _shut_command
+
+	jmp loop
+
+	_help_command:
+		call cmd_help
+		jmp loop
+
+	_shut_command:
+		call cmd_shut
+		jmp loop
 
 ;ниже заморозки можно разместить данные...
 invite_string db ">>> ", 0
@@ -33,8 +46,13 @@ invite_string_end: ;чтобы найти длинну строки
 line_number db 0
 input_char db 0
 
+;буква команды + текст вывода
+help_command_key db 104 ;h в формате ascii
+help_command_string db "List of CMDs", 13, 10, "h => shows this message", 13, 10, "s => shutdown your system", 0
+help_command_string_end:
 
-help_command db 86 ;h в формате ascii
+shut_command_key db 115 ;s в формате ascii
+
 
 ;...и функции
 print:
@@ -53,7 +71,7 @@ print:
 	ret
 
 carriage_return:
-	mov ah, 1
+	mov ah, 01h
 	add [line_number], ah
 	ret
 
@@ -63,7 +81,7 @@ is_it_last_line:
 	je _yes
 	jmp _end
 	_yes:
-	mov ah, 0
+	mov ah, 00h
 	mov [line_number], ah
 	call clear
 	_end:
@@ -76,7 +94,23 @@ clear:
 	ret
 
 read_key:
-	mov ah, 0h
+	mov ah, 00h
 	int 16h
 	mov [input_char], al
+	ret
+
+cmd_help:
+	mov cx, help_command_string_end - help_command_string
+	mov dl, 00h
+	mov bp, help_command_string
+	call print
+	mov ah, 0
+	call carriage_return
+	call carriage_return
+	call carriage_return
+	;yes 3 lines
+	ret
+
+cmd_shut:
+	hlt ;интересно чо буит с компом если так завершить ос?))
 	ret
